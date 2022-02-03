9647251175c7142a5fb101539a5e63be0c67d5e8
muzeffertagiyev <t.muzeffer270@gmail.com>
2022-02-03 21:23:31 +0400

    new files crated and we started to python API lessons

---
+++ freeCodeCamp/python_API_development/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my api"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your post"}

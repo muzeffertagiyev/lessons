22933d37c0a86212955bb2c97ef238c8c8967a65
muzeffertagiyev <t.muzeffer270@gmail.com>
2022-02-06 14:32:32 +0400

    i started to new javascriot course

---
+++ freeCodeCamp/javascript/javascript_programming_fullcourse/people_counter.js
@@ -0,0 +1,30 @@
+
+// let countEl=document.getElementById('count-el');
+// let count=0
+
+
+// function increment(){
+//     count++
+//     countEl.innerText=count;
+
+
+// }
+
+
+// let name='Muzeffer';
+// let greet='Hi, my name is ';
+
+// let myGreeting= greet+ name;
+
+
+// console.log(myGreeting)
+
+
+let welcomeEl=document.getElementById('welcome-el');
+
+let name='Muzeffer';
+let greeting='Hi,My name is ';
+
+welcomeEl.innerHTML=greeting+name
+
+
